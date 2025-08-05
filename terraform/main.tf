6357dbeb63b7de89bebd44e4dad172252ef85ffa
Hauwa Kulu Njidda <hauwa.dbtech@gmail.com>
2025-08-05 11:04:49 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,31 +2,35 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "random_id" "bucket_suffix" {
+  byte_length = 4
+}
+
 resource "aws_s3_bucket" "telco_data" {
-  bucket = "telco-churn-data-buckets"
+  bucket = "telco-churn-data-hauwa"
+
+  tags = {
+    Name = "Telco Churn Data Bucket"
+  }
 }
 
 resource "aws_iam_role" "glue_service_role" {
   name = "glue_service_role"
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [{
-    "Action": "sts:AssumeRole",
-    "Principal": {
-      "Service": "glue.amazonaws.com"
-    },
-    "Effect": "Allow",
-    "Sid": ""
-  }]
-}
-EOF
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "glue.amazonaws.com"
+        }
+      }
+    ]
+  })
 }
 
 resource "aws_iam_role_policy_attachment" "glue_policy" {
   role       = aws_iam_role.glue_service_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
 }
-
-# Glue Job setup will be dynamic – we'll reference this role
-
