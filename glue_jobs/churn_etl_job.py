a45a7365229e4164c5018d17e73701a20f6a7db8
Hauwa Kulu Njidda <hauwa.dbtech@gmail.com>
2025-08-05 10:43:04 +0100

    Update churn_etl_job.py

--- glue_jobs/churn_etl_job.py
+++ glue_jobs/churn_etl_job.py
@@ -1,1 +1,16 @@
+import sys
+from awsglue.context import GlueContext
+from pyspark.context import SparkContext
+from awsglue.dynamicframe import DynamicFrame
+from awsglue.job import Job
+
+glueContext = GlueContext(SparkContext.getOrCreate())
+spark = glueContext.spark_session
+job = Job(glueContext)
+
+df = spark.read.csv("s3://telco-churn-data-buckets/raw/WA_Fn-UseC_-Telco-Customer-Churn.csv", header=True)
+df = df.dropna()  # Remove nulls
+
+df.write.parquet("s3://telco-churn-data-buckets/processed/", mode="overwrite")
+job.commit()
 
