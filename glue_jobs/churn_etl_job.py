5664784c284214b2784abf1eb0473b66d2490d81
Hauwa Kulu Njidda <hauwa.dbtech@gmail.com>
2025-08-05 11:14:23 +0100

    Update churn_etl_job.py

--- glue_jobs/churn_etl_job.py
+++ glue_jobs/churn_etl_job.py
@@ -1,16 +1,26 @@
 import sys
 from awsglue.context import GlueContext
 from pyspark.context import SparkContext
-from awsglue.dynamicframe import DynamicFrame
+from awsglue.utils import getResolvedOptions
 from awsglue.job import Job
 
-glueContext = GlueContext(SparkContext.getOrCreate())
+# Initialize contexts
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+sc = SparkContext()
+glueContext = GlueContext(sc)
 spark = glueContext.spark_session
 job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
 
-df = spark.read.csv("s3://telco-churn-data-buckets/raw/WA_Fn-UseC_-Telco-Customer-Churn.csv", header=True)
-df = df.dropna()  # Remove nulls
+# Load CSV from S3
+input_path = "s3://telco-churn-data-hauwa/raw/WA_Fn-UseC_-Telco-Customer-Churn.csv"
+df = spark.read.option("header", True).csv(input_path)
 
-df.write.parquet("s3://telco-churn-data-buckets/processed/", mode="overwrite")
+# Simple data cleaning: remove rows with nulls
+df_clean = df.dropna()
+
+# Save to S3 in Parquet format
+output_path = "s3://telco-churn-data-hauwa/processed/"
+df_clean.write.mode("overwrite").parquet(output_path)
+
 job.commit()
-
